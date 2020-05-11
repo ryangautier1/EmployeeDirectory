40b58758391cf5cb0eea83e10e68d7495d93b396
Ryan Gautier <ryangautier2@gmail.com>
2020-05-11 18:50:13 -0500

    add react app files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Employees from './components/Employees';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Employees />
+    </div>
+  );
+}
+
+export default App;
