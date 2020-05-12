05297deb5901c82ef09d21b1565a560dcf68fb80
Ryan Gautier <ryangautier2@gmail.com>
2020-05-12 18:07:12 -0500

    sortAlphDec function works for name and role

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Employees from './components/Employees';
 import Navbar from './components/Navbar';
 import employeedata from "./employeedata.json";
-import sortItems from "./sort.js"
+// import sortItems from "./sort.js"
 
 // const salary = [];
-// employeedata.forEach(element => {
+// employeedata.forEach(element => { 
 //   salary.push(element.salary);
 // });
 
@@ -14,16 +14,53 @@ import sortItems from "./sort.js"
 
 class App extends Component {
   state = {
-    employeedata
+    employeedata: employeedata
   };
 
-
+  // takes array of words to sort by, updates state with sorted employeedata
+  sortAlphDec = (data) => {
+    let dataUnsorted = data.map(item => item);
+    let dataSorted = data;
+    dataSorted.sort();
+    let order=[];
+    data.forEach(item => {
+      order.push(dataUnsorted.indexOf(item));
+    });
+    console.log("order: " + order);
+    const newdata = order.map(item => {
+      return this.state.employeedata[item];
+    })
+    console.log(newdata);
+    return this.setState({ employeedata: newdata });
+  }
+  
+  // sortAlphInc = (data) => {
+  //   let order = this.sortAlphDec(data);
+  //   return order.reverse();
+  // }
+  
+  // sortNumDec = (data) => {
+  //   let numsUnsorted= data;
+  //   let numsSorted= data;
+  //   numsSorted.sort();
+  //   let order=[];
+  //   numsSorted.forEach(num => {
+  //     order.push(numsUnsorted.indexOf(num));
+  //   })
+  //   return order;
+  // }
+  
+  // sortNumInc = (data) => {
+  //   let order = this.sortNumDec(data);
+  //   return order.reverse();
+  // }
+  
 
   render() {
     return (
       <div>
         <Navbar />
-        <Employees data={this.state.employeedata} />
+        <Employees data={this.state.employeedata} sortAlphDec={this.sortAlphDec} />
       </div>
     );
   }
--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,5 +1,4 @@
 import React from "react";
-import employeedata from "../employeedata.json";
 import EmployeeRow from "./EmployeeRow";
 
 
@@ -29,8 +28,8 @@ function Employees(props) {
   return (
     <div className="mt-5 container">
       <div className="row py-2 border-top">
-        <h4 className="col-3 text-center" id="name">Name</h4>
-        <h4 className="col-3 text-center" id="position">Position</h4>
+        <h4 className="col-3 text-center" id="name" onClick={() => {props.sortAlphDec(props.data.map(item => item.name))}}>Name</h4>
+        <h4 className="col-3 text-center" id="position" onClick={() => {props.sortAlphDec(props.data.map(item => item.role))}}>Position</h4>
         <h4 className="col-3 text-center" id="salary">Salary</h4>
         <h4 className="col-3 text-center" id="manager">Manager</h4>
       </div>
